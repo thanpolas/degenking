dff2826e154f8041ce3895fddf934bbf763889b1
Thanos Polychronakis <thanpolas@gmail.com>
2022-01-26 16:36:23 +0200

    add getProvider() on configuration

--- src/configure.js
+++ src/configure.js
@@ -2,12 +2,24 @@
  * @fileoverview Configuration of dfk-heroes.
  */
 
+const { ethers } = require('ethers');
+
 /**
  * @type {Object} Local, default configuration.
  */
 exports._config = {
   maxRetries: 6,
   concurrentBlockChainRequests: 30,
+  getProvider: () => {
+    const provider = new ethers.providers.JsonRpcProvider(
+      'https://api.harmony.one/',
+    );
+
+    return {
+      name: 'Official',
+      provider,
+    };
+  },
 };
 
 /**
