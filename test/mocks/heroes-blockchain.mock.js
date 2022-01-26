20e59fbe82d96b8c49d1fe37090133982b5053cf
Thanos Polychronakis <thanpolas@gmail.com>
2022-01-26 17:05:30 +0200

    rename and finalize heroes blockchain mock

---
+++ test/mocks/heroes-blockchain.mock.js
@@ -0,0 +1,38 @@
+/**
+ * @fileoverview Mocks heroes Blockchain related fetching functions.
+ */
+
+const etherEnt = require('../../src/ether/ether.ent');
+
+const {
+  heroChainFix,
+  heroOwnerFix,
+  userHeroesFix,
+} = require('../fixtures/heroes.fix');
+
+const { getContractHeroes } = etherEnt;
+
+/**
+ * Mocks heroes blockchain fetching for testing.
+ *
+ * @return {Object} The mock objects.
+ */
+exports.heroesBlockchainMock = () => {
+  const heroContractFunctionsMock = {
+    getHero: jest.fn(() => Promise.resolve(heroChainFix())),
+    ownerOf: jest.fn(() => Promise.resolve(heroOwnerFix())),
+    getUserHeroes: jest.fn(() => Promise.resolve(userHeroesFix())),
+  };
+
+  const heroContractMock = jest.fn(() =>
+    Promise.resolve(heroContractFunctionsMock),
+  );
+
+  etherEnt.getContractHeroes = heroContractMock;
+
+  return { heroContractMock, heroContractFunctionsMock };
+};
+
+exports.heroesBlockchainMockRestore = () => {
+  etherEnt.getContractHeroes = getContractHeroes;
+};
