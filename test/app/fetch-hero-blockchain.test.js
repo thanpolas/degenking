24f0093cabafb712c420b9c212f36c4147c2c060
Thanos Polychronakis <thanpolas@gmail.com>
2022-01-26 17:27:18 +0200

    finalize fetch-hero-blockchain test

--- test/app/fetch-hero-blockchain.test.js
+++ test/app/fetch-hero-blockchain.test.js
@@ -5,6 +5,12 @@
 const testLib = require('../lib/tester.lib');
 const { getHeroesChain } = require('../..');
 
+const heroesChainMock = require('../mocks/heroes-blockchain.mock');
+const profileChainMock = require('../mocks/profile-blockchain.mock');
+const auctionsChainMock = require('../mocks/auctions-blockchain.mock');
+
+const { assert: assertHero } = require('../assert/normalised-hero.assert');
+
 describe('Fetch Hero Blockchain', () => {
   testLib.init();
 
@@ -12,6 +18,9 @@ describe('Fetch Hero Blockchain', () => {
   const expectedValues = () => ({
     type: 'chain',
     id: 10000,
+    ownerId: 8942,
+    ownerName: 'doody',
+    ownerAddress: '0x2d3c602ff8a024240e25fed8f5772e0b50db7f2d',
     mainClass: 'pirate',
     subClass: 'warrior',
     profession: 'mining',
@@ -20,15 +29,19 @@ describe('Fetch Hero Blockchain', () => {
     maxSummons: 8,
     statBoost1: 'INT',
     statBoost2: 'DEX',
+    active1: 'Basic8',
+    active2: 'Basic3',
+    passive1: 'Basic5',
+    passive2: 'Basic2',
     rarity: 2,
     rarityStr: 'Rare',
-    mining: 2,
+    mining: 5.9,
     gardening: 0,
     foraging: 0.2,
-    fishing: 0,
+    fishing: 2.4,
     shiny: false,
-    xp: 911,
-    level: 1,
+    xp: 914,
+    level: 4,
     statGenes: {
       class: 'pirate',
       subClass: 'warrior',
@@ -65,30 +78,59 @@ describe('Fetch Hero Blockchain', () => {
     nextSummonTime: new Date('2021-10-31T10:05:41.000Z'),
     summonerId: 7863,
     assistantId: 9280,
-    staminaFullAt: new Date('2021-12-13T01:33:37.000Z'),
+    staminaFullAt: new Date('2022-01-26T16:13:19.000Z'),
     hpFullAt: new Date('1970-01-01T00:00:00.000Z'),
     mpFullAt: new Date('1970-01-01T00:00:00.000Z'),
     currentQuest: '0x0000000000000000000000000000000000000000',
     sp: 0,
     status: 0,
-
     intelligence: 7,
-    luck: 10,
-    vitality: 9,
-    dexterity: 9,
-    mp: 30,
-    strength: 10,
-    wisdom: 6,
+    luck: 12,
+    vitality: 13,
+    dexterity: 13,
+    strength: 16,
+    wisdom: 7,
     agility: 8,
-    endurance: 7,
-    hp: 145,
-    stamina: 25,
+    endurance: 11,
+    statsSum: 87,
+    hp: 267,
+    mp: 43,
+    stamina: 27,
+    onSale: false,
+    auctionId: 0,
+    seller: '',
+    startingPrice: 0,
+    endingPrice: 0,
+    startingPriceFormatted: '0',
+    endingPriceFormatted: '0',
+    duration: 0,
+    startedAt: new Date('1970-01-01T00:00:00.000Z'),
+    summonCost: 26,
+    classTier: 'basic',
+    summonMinTears: 10,
+    summonMaxTears: '0basic',
+    currentStamina: 24,
+    currentRank: 47,
+    estJewelPerTick: 0.33498,
+    estJewelPer100Ticks: 33.4982,
+    mainClassGenes: ['pirate', 'warrior', 'wizard', 'thief'],
+    subClassGenes: ['warrior', 'pirate', 'wizard', 'monk'],
+    professionGenes: ['mining', 'gardening', 'mining', 'foraging'],
   });
 
   describe('Happy Path', () => {
     it('should fetch hero 10000', async () => {
-      const hero = await getHeroesChain(10000);
-      console.log('hero:', hero);
+      heroesChainMock.heroesBlockchainMock();
+      profileChainMock.profileBlockchainMock();
+      auctionsChainMock.auctionSalesBlockchainMock();
+
+      const [hero] = await getHeroesChain([10000]);
+
+      assertHero(hero, expectedValues);
+
+      heroesChainMock.heroesBlockchainMockRestore();
+      profileChainMock.profileBlockchainMockRestore();
+      auctionsChainMock.auctionSalesBlockchainMockRestore();
     });
   });
 });
