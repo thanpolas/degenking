8403b27f57bf00cb003477fefc96d5f056198f30
Thanos Polychronakis <thanpolas@gmail.com>
2022-01-26 13:37:59 +0200

    create addresses constants

---
+++ src/constants/addresses.const.js
@@ -0,0 +1,7 @@
+/**
+ * @fileoverview In game contract addresses.
+ */
+
+exports.HEROES_NFT = '0x5F753dcDf9b1AD9AabC1346614D1f4746fd6Ce5C';
+exports.AUCTION_SALES = '0x13a65b9f8039e2c032bc022171dc05b30c3f2892';
+exports.PROFILES = '0xabD4741948374b1f5DD5Dd7599AC1f85A34cAcDD';
