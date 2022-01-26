66bcc8c84e34b444571169973e03da050cf1751d
Thanos Polychronakis <thanpolas@gmail.com>
2022-01-26 13:37:31 +0200

    bootstrap module

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-module.exports = require('./heroes.ent');
+/**
+ * DeFi Kingdoms Hero
+ * Utility library for fetching and working with DFK Heroes.
+ *
+ * https://github.com/degen-heroes/dfk-hero
+ *
+ * Copyright © Thanos Polychronakis
+ * LICENSE on /LICENSE file.
+ */
+
+/**
+ * @fileoverview bootstrap and master exporting module.
+ */
+
+const logService = require('./utils/log.service');
+
+const logParams = {
+  appName: 'dfk-hero',
+};
+logService.init(logParams);
