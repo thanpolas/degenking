d879bc65708632836d666eba79ae3ce7417a3a08
Thanos Polychronakis <thanpolas@gmail.com>
2022-04-15 20:18:27 +0300

    implement assisting auctions fetching

--- src/auctions/assisting-auctions.ent.js
+++ src/auctions/assisting-auctions.ent.js
@@ -13,17 +13,77 @@ exports.MAX_RECORDS = 1000;
 /**
  * Query for all rental market via GQL, will fetch all records.
  *
+ * @param {Object=} optCriteria Optionally define criteria.
+ * @return {Promise<Array<Object>>} Array with normalized heroes.
+ */
+exports.queryAssistingAuctionsAllGql = async (optCriteria) => {
+  let criteria = null;
+  if (optCriteria) {
+    criteria = exports._parseCriteria(optCriteria);
+  }
+
+  return exports._queryActual(criteria);
+};
+
+/**
+ * Will parse and convert criteria to GQL filtering where clause.
+ *
+ * @param {Object} criteria The criteria to filter with.
+ * @return {string} GQL  Where clause.
+ * @private
+ */
+exports._parseCriteria = (criteria) => {
+  // First empty string required to force a comma at the start
+  const gqlQueryAr = [''];
+
+  if (criteria.mainClass) {
+    if (Array.isArray(criteria.mainClass)) {
+      gqlQueryAr.push(`mainClass_in: ["${criteria.mainClass.join('", "')}"]`);
+    } else {
+      gqlQueryAr.push(`mainClass: "${criteria.mainClass}"`);
+    }
+  }
+  if (criteria.subClass) {
+    gqlQueryAr.push(`subClass: "${criteria.subClass}"`);
+  }
+
+  if (Number.isInteger(criteria.generation)) {
+    if (Array.isArray(criteria.generation)) {
+      gqlQueryAr.push(`generation_in: [${criteria.generation.join(',')}]`);
+    } else {
+      gqlQueryAr.push(`generation: ${criteria.generation}`);
+    }
+  }
+
+  if (criteria.profession) {
+    if (Array.isArray(criteria.profession)) {
+      gqlQueryAr.push(`profession_in: ["${criteria.profession.join('", "')}"]`);
+    } else {
+      gqlQueryAr.push(`profession: "${criteria.profession}"`);
+    }
+  }
+
+  const gqlWhere = gqlQueryAr.join(',');
+  return gqlWhere;
+};
+
+/**
+ * Actual query for all rental market via GQL, will recurse to fetch all records.
+ *
+ * @param {string|null} criteria Filtering string or null for no filter.
  * @param {number=} first Overwrite to fetch less heroes.
  * @param {number=} skip Used for recurssion to fetch all records.
  * @param {Array<Object>=} heroesFetched Execution state containing all fetched heroes.
  * @return {Promise<Array<Object>>} Array with normalized heroes.
+ * @private
  */
-exports.queryAssistingAuctionsAllGql = async (
+exports._queryActual = async (
+  criteria,
   first = exports.MAX_RECORDS,
   skip = 0,
   heroesFetched = [],
 ) => {
-  const res = await gqlQuery(assistingAuctions, {
+  const res = await gqlQuery(assistingAuctions(criteria), {
     first,
     skip,
   });
@@ -39,5 +99,5 @@ exports.queryAssistingAuctionsAllGql = async (
   }
 
   const nextSkip = skip + exports.MAX_RECORDS;
-  return exports.queryAssistingAuctionsAllGql(first, nextSkip, allHeroes);
+  return exports._queryActual(criteria, first, nextSkip, allHeroes);
 };
