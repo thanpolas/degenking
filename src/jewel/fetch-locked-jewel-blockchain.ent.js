4768243f26f4e3ec6eb48bcc01af79a033bcae02
Thanos Polychronakis <thanpolas@gmail.com>
2022-06-22 15:33:12 +0300

    use error handler module on locked jewel query

--- src/jewel/fetch-locked-jewel-blockchain.ent.js
+++ src/jewel/fetch-locked-jewel-blockchain.ent.js
@@ -5,10 +5,9 @@
 
 const { tokenToAuto } = require('@thanpolas/crypto-utils');
 
-const { get: getConfig } = require('../configure');
 const { JEWEL_DECIMALS } = require('../constants/constants.const');
 const { getProvider, getContractJewel } = require('../ether');
-const { delay } = require('../utils/helpers');
+const { catchErrorRetry } = require('../utils/error-handler');
 
 const log = require('../utils/log.service').get();
 
@@ -16,12 +15,12 @@ const log = require('../utils/log.service').get();
  * Fetches locked jewel by owner.
  *
  * @param {string} ownerAddress The owner's address to fetch - lowercased.
- * @param {number=} optRetry Retry count.
+ * @param {number=} retries Retry count.
  * @return {Promise<Object>} Fetched jewel.
  */
-exports.fetchLockedJewelByOwnerChain = async (ownerAddress, optRetry = 0) => {
+exports.fetchLockedJewelByOwnerChain = async (ownerAddress, retries = 0) => {
+  const currentRPC = await getProvider();
   try {
-    const currentRPC = await getProvider();
     const jewelContract = await getContractJewel(currentRPC);
 
     const userLockedJewel = await jewelContract.lockOf(ownerAddress);
@@ -32,18 +31,13 @@ exports.fetchLockedJewelByOwnerChain = async (ownerAddress, optRetry = 0) => {
 
     return normalizedUserLockedJewel;
   } catch (ex) {
-    const currentRPC = await getProvider();
-    optRetry += 1;
-    if (optRetry > getConfig('maxRetries')) {
-      throw ex;
-    }
-    await log.error(
-      `Failed to fetch locked jewel for owner ${ownerAddress} - retry: ` +
-        `${optRetry} - RPC: ${currentRPC.name}`,
-      { error: ex },
-    );
-
-    await delay(optRetry);
-    return exports.fetchLockedJewelByOwnerChain(ownerAddress, optRetry);
+    await catchErrorRetry(log, {
+      ex,
+      retries,
+      errorMessage: 'fetchLockedJewelByOwnerChain()',
+      retryFunction: exports.fetchLockedJewelByOwnerChain,
+      retryArguments: [ownerAddress],
+      doNotThrow: null,
+    });
   }
 };
