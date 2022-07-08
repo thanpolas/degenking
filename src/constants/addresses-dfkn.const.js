bc271a6550d9bc2ff43d940e80bea5d6799d6c45
Thanos Polychronakis <thanpolas@gmail.com>
2022-07-08 16:17:28 +0300

    populate CV addresses

---
+++ src/constants/addresses-dfkn.const.js
@@ -0,0 +1,52 @@
+/**
+ * @fileoverview Standardised naming of contracts for DFK Network.
+ */
+
+exports.UNISWAPV2FACTORY = '';
+exports.UNISWAPV2ROUTER = '';
+exports.MASTER_GARDENER = '';
+exports.JEWEL_TOKEN = '0xCCb93dABD71c8Dad03Fc4CE5559dC3D89F67a260';
+exports.CRYSTAL_TOKEN = '0x04b9dA42306B023f3572e106B11D82aAd9D32EBb';
+exports.XJEWEL_TOKEN = '0x77f2656d04E158f915bC22f07B779D94c1DC47Ff';
+exports.XCRYSTAL_TOKEN = '0x6e7185872bcdf3f7a6cbbe81356e50daffb002d2';
+
+exports.BANK = '';
+exports.BANKER = '';
+exports.AIRDROP = '';
+
+exports.HEROES = '0xEb9B61B145D6489Be575D3603F4a704810e143dF';
+exports.TEARS_TOKEN = '0x58E63A9bbb2047cd9Ba7E6bB4490C238d271c278';
+exports.AUCTION_SALES = '';
+exports.AUCTION_SALES_LOWERCASE = '';
+exports.PROFILES = '';
+exports.RUNES = '';
+exports.MEDITATION = '';
+exports.SUMMON_V2 = '';
+
+//
+// QUESTING
+//
+
+// Currently active quests
+exports.QUEST_CORE_V2 = '';
+exports.QUEST_GARDENING_V1 = '';
+exports.QUEST_MINING_GOLD_V1 = '';
+exports.QUEST_MINING_JEWEL_V1 = '';
+exports.QUEST_FORAGING_V2 = '';
+exports.QUEST_FISHING_V2 = '';
+exports.QUEST_WISHING_WELL_V2 = '';
+
+//
+// Training Quest Contracts
+//
+
+exports.QUESTS_REV = {};
+
+/** @const {Array<string>} TRAINING_QUESTS All the training quests */
+exports.TRAINING_QUESTS_AR = [];
+
+/** @enum {boolean} Determines which quest types need to be handled by new questing */
+exports.QUESTS_HANDLER_NEW = {};
+
+/** @enum {Array<string>} Map profession values to profession quest addresses */
+exports.PROFESSIONS_TO_QUESTS = {};
