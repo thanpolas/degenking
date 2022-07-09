f252fecb1ad4825b2cdb4d7eea7a5a9f27bc5225
Thanos Polychronakis <thanpolas@gmail.com>
2022-07-09 11:54:20 +0300

    make fetching locked game tokens generic

--- src/tokens/fetch-locked-tokens-blockchain.ent.js
+++ src/tokens/fetch-locked-tokens-blockchain.ent.js
@@ -5,28 +5,34 @@
 
 const { tokenToAuto } = require('@thanpolas/crypto-utils');
 
-const { JEWEL_DECIMALS, NETWORK_IDS } = require('../constants/constants.const');
-const { getProvider, getContractJewel } = require('../ether');
+const { JEWEL_DECIMALS } = require('../constants/constants.const');
+const { getProvider, getContractGameToken } = require('../ether');
 const { catchErrorRetry } = require('../utils/error-handler');
 
 const log = require('../utils/log.service').get();
 
 /**
- * Fetches locked jewel by owner.
+ * Fetches locked game tokens by owner, will return the appropriate
+ * locked amount depending on the network (chainId).
  *
+ * @param {number} chainId Chain id.
  * @param {string} ownerAddress The owner's address to fetch - lowercased.
  * @param {number=} retries Retry count.
  * @return {Promise<Object>} Fetched jewel.
  */
-exports.fetchLockedJewelByOwnerChain = async (ownerAddress, retries = 0) => {
-  const currentRPC = await getProvider(NETWORK_IDS.HARMONY);
+exports.fetchLockedTokensByOwnerChain = async (
+  chainId,
+  ownerAddress,
+  retries = 0,
+) => {
+  const currentRPC = await getProvider(chainId);
   try {
-    const jewelContract = await getContractJewel(currentRPC);
+    const gameTokenContract = await getContractGameToken(currentRPC);
 
-    const userLockedJewel = await jewelContract.lockOf(ownerAddress);
+    const userLockedGameTokens = await gameTokenContract.lockOf(ownerAddress);
 
     const normalizedUserLockedJewel = Number(
-      tokenToAuto(userLockedJewel, JEWEL_DECIMALS),
+      tokenToAuto(userLockedGameTokens, JEWEL_DECIMALS),
     );
 
     return normalizedUserLockedJewel;
@@ -34,9 +40,9 @@ exports.fetchLockedJewelByOwnerChain = async (ownerAddress, retries = 0) => {
     await catchErrorRetry(log, {
       ex,
       retries,
-      errorMessage: `fetchLockedJewelByOwnerChain() - RPC: ${currentRPC.name}`,
-      retryFunction: exports.fetchLockedJewelByOwnerChain,
-      retryArguments: [ownerAddress],
+      errorMessage: `fetchLockedTokensByOwnerChain() - RPC: ${currentRPC.name}`,
+      retryFunction: exports.fetchLockedTokensByOwnerChain,
+      retryArguments: [chainId, ownerAddress],
       doNotThrow: null,
     });
   }
