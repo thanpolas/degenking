e8ea1af0ecedd5b9804896abbcd4f17a9590bf3e
Thanos Polychronakis <thanpolas@gmail.com>
2022-07-11 17:46:45 +0300

    create chainIdToRealm() fn

--- src/utils/network-helpers.js
+++ src/utils/network-helpers.js
@@ -2,7 +2,10 @@
  * @fileoverview Generic error helpers.
  */
 
-const { NETWORK_IDS_REV } = require('../constants/constants.const');
+const {
+  NETWORK_IDS_REV,
+  NETWORK_IDS_REV_REALM,
+} = require('../constants/constants.const');
 
 /**
  * Converts chain id to human readable.
@@ -12,4 +15,14 @@ const { NETWORK_IDS_REV } = require('../constants/constants.const');
  */
 exports.chainIdToNetwork = (chainId) => {
   return NETWORK_IDS_REV[chainId];
+};
+
+/**
+ * Converts chain id to human readable DFK Realm (SD or CV).
+ *
+ * @param {number} chainId The chain id.
+ * @return {string}
+ */
+exports.chainIdToRealm = (chainId) => {
+  return NETWORK_IDS_REV_REALM[chainId];
 };
