24ca27b1cb703a77a22eea00fde6b485eb522d26
Thanos Polychronakis <thanpolas@gmail.com>
2022-09-21 13:16:46 +0300

    create visual genes fixtures

---
+++ test/fixtures/visual-genes.fix.js
@@ -0,0 +1,34 @@
+/**
+ * @fileoverview Visual Genes stats.
+ */
+
+exports.hero300094VisualGenes = () => {
+  return '0x08423118a629848628c022489204e9210813104203844390c13a0c3188e2';
+};
+
+exports.hero300094ExpectedValues = () => {
+  return {
+    gender: 'female',
+    background: 'desert',
+    hairStyle: 'Lamia (B10)',
+    hairColorDescr: 'Basic10',
+    hairColor: '326988',
+    headAppendage: 6,
+    headAppendageDescr: 'Minotaur Horns (B7)',
+    headAppendageColor: 'Basic4',
+    skinColor: '985e1c',
+    eyeColorNum: 2,
+    eyeColor: '896693',
+    visualUnknown1: 1,
+    visualUnknown1Descr: 'Basic2',
+    visualUnknown2: 2,
+    visualUnknown2Descr: 'Basic3',
+
+    appendageColor: 'a88b47',
+
+    backAppendage: 8,
+    backAppendageDescr: 'Skeletal Wings (B9)',
+    backAppendageColor: '566f7d',
+    backAppendageColorDescr: 'Basic4',
+  };
+};
