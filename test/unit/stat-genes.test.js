5c35f23e09488817d5cdff2f0246ebc8b311f280
Thanos Polychronakis <thanpolas@gmail.com>
2022-09-21 15:50:38 +0300

    expand on stat gene tests

--- test/unit/stat-genes.test.js
+++ test/unit/stat-genes.test.js
@@ -6,16 +6,76 @@ const testLib = require('../lib/tester.lib');
 const {
   decodeStatGenes,
 } = require('../../src/heroes-helpers/decode-genes.ent');
+const { hero1000000000088StatGenes } = require('../fixtures/stat-genes.fix');
 
 describe('Stat Genes Decoding', () => {
   testLib.init();
   describe('Happy Path', () => {
-    test('Will decode main and subclass of new Sheer class', () => {
-      const statGenes =
-        '283244464718120278602770687730475582917747007096713223029150594578692';
+    test('Will decode Dominant stat genes for hero id 1000000000088', () => {
+      const statGenes = hero1000000000088StatGenes();
 
       const statGenesDecoded = decodeStatGenes(statGenes);
-      expect(statGenesDecoded.class).toEqual('berserker');
+
+      expect(statGenesDecoded.class).toEqual(8);
+      expect(statGenesDecoded.subClass).toEqual(9);
+      expect(statGenesDecoded.profession).toEqual(4);
+      expect(statGenesDecoded.passive1).toEqual(3);
+      expect(statGenesDecoded.passive2).toEqual(7);
+      expect(statGenesDecoded.active1).toEqual(5);
+      expect(statGenesDecoded.active2).toEqual(3);
+      expect(statGenesDecoded.statBoost1).toEqual(6);
+      expect(statGenesDecoded.statBoost2).toEqual(10);
+      expect(statGenesDecoded.statsUnknown1).toEqual(3);
+      expect(statGenesDecoded.element).toEqual(8);
+      expect(statGenesDecoded.statsUnknown2).toEqual(10);
+      expect(statGenesDecoded.classDescr).toEqual('berserker');
+      expect(statGenesDecoded.subClassDescr).toEqual('seer');
+      expect(statGenesDecoded.professionDescr).toEqual('fishing');
+      expect(statGenesDecoded.passive1Mut).toEqual('Basic4');
+      expect(statGenesDecoded.passive2Mut).toEqual('Basic8');
+      expect(statGenesDecoded.active1Mut).toEqual('Basic6');
+      expect(statGenesDecoded.active2Mut).toEqual('Basic4');
+      expect(statGenesDecoded.statsUnknown1Mut).toEqual('Basic4');
+      expect(statGenesDecoded.statsUnknown2Mut).toEqual('Basic11');
+      expect(statGenesDecoded.statBoost1Descr).toEqual('WIS');
+      expect(statGenesDecoded.statBoost2Descr).toEqual('VIT');
+      expect(statGenesDecoded.elementDescr).toEqual('lightning');
+    });
+
+    test('Will decode visual Recessive 1 genes for hero id 1000000000088', () => {
+      const statGenes = hero1000000000088StatGenes();
+
+      const statGenesDecoded = decodeStatGenes(statGenes);
+
+      expect(statGenesDecoded.recessives).toBeObject();
+      const { recessives } = statGenesDecoded;
+      expect(recessives).toContainAllKeys(['r1', 'r2', 'r3']);
+      const { r1 } = recessives;
+
+      expect(r1.class).toEqual(5);
+      expect(r1.subClass).toEqual(0);
+      expect(r1.profession).toEqual(2);
+      expect(r1.passive1).toEqual(3);
+      expect(r1.passive2).toEqual(2);
+      expect(r1.active1).toEqual(2);
+      expect(r1.active2).toEqual(7);
+      expect(r1.statBoost1).toEqual(10);
+      expect(r1.statBoost2).toEqual(14);
+      expect(r1.statsUnknown1).toEqual(4);
+      expect(r1.element).toEqual(0);
+      expect(r1.statsUnknown2).toEqual(0);
+      expect(r1.classDescr).toEqual('wizard');
+      expect(r1.subClassDescr).toEqual('warrior');
+      expect(r1.professionDescr).toEqual('gardening');
+      expect(r1.passive1Mut).toEqual('Basic4');
+      expect(r1.passive2Mut).toEqual('Basic3');
+      expect(r1.active1Mut).toEqual('Basic3');
+      expect(r1.active2Mut).toEqual('Basic8');
+      expect(r1.statsUnknown1Mut).toEqual('Basic5');
+      expect(r1.statsUnknown2Mut).toEqual('Basic1');
+      expect(r1.statBoost1Descr).toEqual('VIT');
+      expect(r1.statBoost2Descr).toEqual('DEX');
+      expect(r1.elementDescr).toEqual('fire');
     });
   });
 });
