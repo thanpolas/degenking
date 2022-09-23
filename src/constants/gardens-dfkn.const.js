5d5f2679692c09ae37ee0d60a5cac4bbe67423fe
Thanos Polychronakis <thanpolas@gmail.com>
2022-09-23 18:29:46 +0300

    fix new pools addresses

--- src/constants/gardens-dfkn.const.js
+++ src/constants/gardens-dfkn.const.js
@@ -53,43 +53,44 @@ exports.Pools = [
   {
     pid: 7,
     pair: 'CRYSTAL-BTC.b',
-    address: '0x00bd81c9bac29a3b6aea7abc92d2c9a3366bb4dd',
+    address: '0x706916dbc3b66d89632708cc193080ea05e0534a',
     active: true,
   },
+
   {
     pid: 8,
     pair: 'CRYSTAL-KLAY',
-    address: '0xafc1fbc3f3fb517eb54bb2472051a6f0b2105320',
+    address: '0x1fcc67a01525fd715a67bccbf73665fb3dbe76c7',
     active: true,
   },
   {
     pid: 9,
     pair: 'JEWEL-KLAY',
-    address: '0x561091e2385c90d41b4c0daef651a4b33e1a5cfe',
+    address: '0x2a70aa48f9dbf859239ae5e7f98fe95ae27a6cd4',
     active: true,
   },
   {
     pid: 10,
     pair: 'JEWEL-AVAX',
-    address: '0xf3eabed6bd905e0fcd68fc3dbcd6e3a4aee55e98',
+    address: '0xa0d17554f09047d65e0ae0e76cd8923a9525183c',
     active: true,
   },
   {
     pid: 11,
     pair: 'JEWEL-BTC.b',
-    address: '0xfaa8507e822397bd56efd4480fb12adc41ff940b',
+    address: '0x3391b9384ac66c7aa3bf4a75a4f441942b1dcf30',
     active: true,
   },
   {
     pid: 12,
     pair: 'JEWEL-ETH',
-    address: '0x79724b6996502afc773feb3ff8bb3c23adf2854b',
+    address: '0xbaec39dd81b964b57bc5fa5f5421cd82185409e6',
     active: true,
   },
   {
     pid: 13,
     pair: 'BTC.b-USDC',
-    address: '0x59d642b471dd54207cb1cde2e7507b0ce1b1a6a5  ',
+    address: '0x045838dbfb8026520e872c8298f4ed542b81eaca  ',
     active: true,
   },
 ];
