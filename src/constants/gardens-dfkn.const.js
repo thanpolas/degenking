5b897781370685a6c17608ce315251b595e0b9da
Thanos Polychronakis <thanpolas@gmail.com>
2022-09-23 17:28:09 +0300

    add new gardens on CV

--- src/constants/gardens-dfkn.const.js
+++ src/constants/gardens-dfkn.const.js
@@ -50,6 +50,48 @@ exports.Pools = [
     address: '0x810e1ff51fdd58c474c66a31013713d1a17bf458',
     active: true,
   },
+  {
+    pid: 7,
+    pair: 'CRYSTAL-BTC.b',
+    address: '0x00bd81c9bac29a3b6aea7abc92d2c9a3366bb4dd',
+    active: true,
+  },
+  {
+    pid: 8,
+    pair: 'CRYSTAL-KLAY',
+    address: '0xafc1fbc3f3fb517eb54bb2472051a6f0b2105320',
+    active: true,
+  },
+  {
+    pid: 9,
+    pair: 'JEWEL-KLAY',
+    address: '0x561091e2385c90d41b4c0daef651a4b33e1a5cfe',
+    active: true,
+  },
+  {
+    pid: 10,
+    pair: 'JEWEL-AVAX',
+    address: '0xf3eabed6bd905e0fcd68fc3dbcd6e3a4aee55e98',
+    active: true,
+  },
+  {
+    pid: 11,
+    pair: 'JEWEL-BTC.b',
+    address: '0xfaa8507e822397bd56efd4480fb12adc41ff940b',
+    active: true,
+  },
+  {
+    pid: 12,
+    pair: 'JEWEL-ETH',
+    address: '0x79724b6996502afc773feb3ff8bb3c23adf2854b',
+    active: true,
+  },
+  {
+    pid: 13,
+    pair: 'BTC.b-USDC',
+    address: '0x59d642b471dd54207cb1cde2e7507b0ce1b1a6a5  ',
+    active: true,
+  },
 ];
 
 exports.PoolsIndexedByPid = indexArrayToObject(exports.Pools, 'pid');
