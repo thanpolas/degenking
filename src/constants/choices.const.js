30af7d412ef59f09f996bcac1d591862dd9eed83
Zelys <junk@elysius.net>
2023-02-01 12:05:45 -0600

    Added SD classes.

--- src/constants/choices.const.js
+++ src/constants/choices.const.js
@@ -24,13 +24,17 @@ exports.class = {
   7: 'pirate',
   8: 'berserker',
   9: 'seer',
+  10: 'legionnaire',
+  11: 'scholar',
   16: 'paladin',
   17: 'darkKnight',
   18: 'summoner',
   19: 'ninja',
   20: 'shapeshifter',
+  21: 'bard',
   24: 'dragoon',
   25: 'sage',
+  26: 'spellbow',
   28: 'dreadKnight',
 };
 exports.skinColor = {
