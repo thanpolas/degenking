3c10161f1d0b8c221094d10d560425f1dc9163d3
Thanos Polychronakis <thanpolas@gmail.com>
2023-05-28 19:16:35 +0300

    create the questData typedef

---
+++ src/type-defs.js
@@ -0,0 +1,22 @@
+/**
+ * Quest data type definition.
+ *
+ * @typedef {Object} questData
+ * @property {number} version Quest core version.
+ * @property {number} chainId The chain id the quest is on.
+ * @property {string} networkName The network name quest is on.
+ * @property {number} questId The quest id.
+ * @property {string} questInstanceId The quest Instance id (V3).
+ * @property {string} questName Human readable quest name.
+ * @property {string} questTypeId The Quest type id.
+ * @property {string} questTypeName The quest type in human readable format.
+ * @property {Array<number>} heroIds Hero ids on this quest.
+ * @property {string} playerAddress 0x address of player.
+ * @property {string} playerAddressLower 0x address of player, lowercased.
+ * @property {number} startBlock Block quest started.
+ * @property {Date} startAtTime When quest started.
+ * @property {Date} completesAtTime When quest completes.
+ * @property {number} attempts Quest attempts.
+ * @property {number} status Raw status value from quest.
+ * @property {number} level Quest level.
+ */
